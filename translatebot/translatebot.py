126512e4aa7f027add9bb01041d63f999f463526
Amir reza Afra <adres.afra@gmail.com>
2023-10-22 00:25:37 +0330

    added choose language menu and error handeler

--- translatebot/translatebot.py
+++ translatebot/translatebot.py
@@ -6,31 +6,35 @@ from telegram.ext import Application, CommandHandler, MessageHandler, ContextTyp
 Token: Final = '6571411673:AAEsrOjiRI-MQQxoGvx-3D2c0mqCp4lW62o'
 Bot_username: Final = '@Translator_pe_bot'
 
-async def select_menu(update : Update , context : CallbackContext):
-    keyboard = {
-        InlineKeyboardButton('hello' , callback_data="hello world")
-    }
+async def select_language(update : Update , context : CallbackContext) -> None:
+    keyboard = [
+        [
+            InlineKeyboardButton("Persian" , callback_data="hello world"),
+            InlineKeyboardButton("Arabic" , callback_data="hello world"),
+            InlineKeyboardButton("Germany" , callback_data="hello world")
+         ]
+    ]
     reply_markup = InlineKeyboardMarkup(keyboard)
-    update.message.reply_text("say hello!" ,reply_markup= reply_markup)
+    await update.message.reply_text("select your language" ,reply_markup= reply_markup)
 
+async def reply(updat : Update , context : ContextTypes.DEFAULT_TYPE):
+    user_input =updat.message.text
+    await updat.message.reply_text(f'you say {user_input}')
 
-async def start_command(update: Update, context : ContextTypes.DEFAULT_TYPE):
-    massage_user :str =update.message.chat.first_name + update.message.chat.last_name
-    await update.message.reply_text(f"hi {massage_user}")
+async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(f'Update {update} causded error {context.error}')
 
-
-async def handle_massage(update: Update, context: ContextTypes.DEFAULT_TYPE):
-
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message_type: str = update.message.chat.type
     text: str = update.message.text  
-
     await update.message.reply_text(f'send me {text}')  
 
 if __name__ == '__main__':
     app = Application.builder().token(Token).build()
 
 
-app.add_handler(CommandHandler('start', start_command))
-app.add_handler(MessageHandler(filters.TEXT, handle_massage))
-print("poling")
-app.run_polling(poll_interval=4)
+    app.add_handler(CommandHandler('select_language', select_language))
+    app.add_handler(MessageHandler(filters.TEXT, reply))
+    app.add_error_handler(error)
+    print("polling")
+    app.run_polling(poll_interval=3)
